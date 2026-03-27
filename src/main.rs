b3499e144f2bc80a3de393e5df70cb2cc8ce1dd4
simon0302010 <simon@hackclub.app>
2026-03-27 15:33:57 +0100

    Grid layout with search bar and map

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use gtk::gdk::Display;
 use shumate::{MapLayer, MapSourceRegistry, prelude::*};
 
 use shumate::{Map};
-use gtk::{Application, ApplicationWindow, glib};
+use gtk::{Application, ApplicationWindow, CssProvider, Grid, SearchBar, SearchEntry, glib};
 
 mod trace;
 
@@ -10,21 +11,44 @@ fn main() -> glib::ExitCode {
         .application_id("de.simon0302010.trazer")
         .build();
 
+    app.connect_startup(|_| load_css());
     app.connect_activate(setup_gui);
 
     app.run()
 }
 
 fn setup_gui(app: &Application) {
+    let grid = Grid::builder()
+        .row_spacing(8).column_spacing(8)
+        .margin_top(12).margin_bottom(12).margin_end(12).margin_start(12)
+        .build();
+
+    let ip_input = SearchEntry::default();
+    grid.attach(&ip_input, 0, 0, 1, 1);
+
     let map = create_map();
+    map.set_hexpand(true); map.set_vexpand(true);
+    map.add_css_class("rounded");
+    grid.attach(&map, 0, 1, 1, 1);
 
     let window = ApplicationWindow::builder()
         .application(app)
         .default_width(400)
-        .child(&map)
+        .child(&grid)
         .build();
 
     window.show();
+}
+
+fn load_css() {
+    let provider = CssProvider::new();
+    provider.load_from_data(include_str!("style.css"));
+
+    gtk::style_context_add_provider_for_display(
+        &Display::default().expect("Could not connect a display"),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
 }
 
 fn create_map() -> Map {
@@ -39,7 +63,8 @@ fn create_map() -> Map {
     map.set_map_source(&source);
     map.add_layer(&MapLayer::new(&source, &viewport));
 
-    viewport.set_zoom_level(3.0);
+    viewport.set_zoom_level(5.0);
+    viewport.set_location(52.520007, 13.404954);
 
     map
 }
